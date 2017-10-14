5c4ba0d4975a1e0d9b872bcbac3e6e53489f93ac
Christian Samuel <christian.leumas@icloud.com>
2017-10-13 22:35:58 -0400

    server logic.

--- server.rb
+++ server.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
+require 'random_restaurant_selector'
 
 
 get '/' do
-  File.read('public/index.html')
+puts request_hash = { location: ENV['MY_LOCATION'], term: 'food', radius: 900, open_now: false, price: '1,2', limit: 50 }
+puts search = RandomRestaurantSelector::Search.new(request_hash)
+puts restaurant = search.gimme_a_restaurant(search.get_businesses)
+puts restaurant.send_to_slack
 end
